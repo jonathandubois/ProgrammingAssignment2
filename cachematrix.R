70c63901dfda5cba7c2fd1316720f3a6ee64a843
jmd458 <jonathandubois@MacBook-Air.local>
2015-05-23 01:54:24 -0400

    changes
    several

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function:
+## This is the stupidest fucking shit I have ever done. It has nothing to do 
+## with the lectures and will not help me in the future, but fuck it, here:
+## The following function creates chaches a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
 
+	get <- function() { 
+		x
+	}
+	
+	setmatrix <- function(solve) {
+		m <<- solve
+	}
+	
+	getmatrix <- function() {
+		m
+	}
+	
+	list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
+## The following function retrieves the inverse matrix created by makeCacheMatrix
 
-## Write a short comment describing this function
+cacheSolve <- function(x=matrix(), ...) {
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getmatrix()
+
+	if(is.null(m)) {
+		message("matrix is null!")
+	} else {
+		return(m)
+	}
+
+	matrix <- x$get()
+	m <- solve(matrix, ...)
+	x$setmatrix(m)
+   
+   return(m)
 }
